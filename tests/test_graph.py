a8c45cd425ef1f669565947e8734b7c83345fa68
Colin Smith <colin.smith@wisc.edu>
2024-09-03 12:35:06 -0700

    refactor: deprecate existing load functions in favor of `load_graph`
    Deprecated the `load_metadata` and `load_vocabularies` functions in
    favor of the `load_graph` function. This unified approach preserves
    blank node references, addressing limitations of the `ConjunctiveGraph`
    object as outlined in rdflib version 7.0.0 documentation.

--- tests/test_graph.py
+++ tests/test_graph.py
@@ -2,25 +2,7 @@
 
 from os import listdir
 import importlib
-from spinneret.graph import load_metadata, load_vocabularies, load_graph
-
-
-def test_combine_jsonld_files():
-    """Test load_metadata"""
-    data_dir = str(importlib.resources.files("spinneret.data")) + "/jsonld"
-    files = [data_dir + "/" + f for f in listdir(data_dir)]
-    res = load_metadata(files)
-    assert res is not None
-    assert len(res) > 0
-
-
-def test_load_vocabularies():
-    """Test load_vocabularies"""
-    data_dir = "tests/data/vocab"
-    files = [data_dir + "/" + f for f in listdir(data_dir)]
-    res = load_vocabularies(files)
-    assert res is not None
-    assert len(res) > 0
+from spinneret.graph import load_graph
 
 
 def test_load_graph():
