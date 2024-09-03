860e7965ba467c9398f06eb988808e48e2f86803
Colin Smith <colin.smith@wisc.edu>
2024-09-03 10:24:29 -0700

    feat: load vocabularies into a graph
    Introduce a function to load target vocabularies into a graph.

--- tests/test_graph.py
+++ tests/test_graph.py
@@ -2,7 +2,7 @@
 
 from os import listdir
 import importlib
-from spinneret.graph import load_metadata
+from spinneret.graph import load_metadata, load_vocabularies
 
 
 def test_combine_jsonld_files():
@@ -10,5 +10,14 @@ def test_combine_jsonld_files():
     data_dir = str(importlib.resources.files("spinneret.data")) + "/jsonld"
     files = [data_dir + "/" + f for f in listdir(data_dir)]
     res = load_metadata(files)
+    assert res is not None
+    assert len(res) > 0
+
+
+def test_load_vocabularies():
+    """Test load_vocabularies"""
+    data_dir = "tests/data/vocab"
+    files = [data_dir + "/" + f for f in listdir(data_dir)]
+    res = load_vocabularies(files)
     assert res is not None
     assert len(res) > 0
