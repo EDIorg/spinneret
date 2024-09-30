144d7b15cc00a7a2f469bbe2d4082ad2eff12faf
Colin Smith <colin.smith@wisc.edu>
2024-09-30 08:34:05 -0700

    feat: ensure EML userId is a URL for linked data compatibility
    Modify the EML userId element to be a URL, whenever possible,
    facilitating linked data compatibility. If the current value isn't a URL,
    it's converted using the directory attribute as a base URL.
    
    This function addresses a practice, previously recommended by EDI, of
    setting the base URL as the directory attribute and the remaining
    identifier as the element value. This practice has been recently
    deprecated.

--- src/spinneret/shadow.py
+++ src/spinneret/shadow.py
@@ -1,1 +1,31 @@
 """A module for creating shadow metadata"""
+
+from urllib.parse import urljoin
+from lxml import etree
+from spinneret.utilities import is_uri
+
+
+def convert_userid_to_url(eml: etree.ElementTree) -> etree.ElementTree:
+    """
+    :param eml: An EML document
+    :returns:   An EML document with userId elements converted to URLs, if
+        not already, and if possible.
+    """
+    # Find all userId elements with a directory attribute
+    userid_elements = eml.xpath("//userId[@directory]")
+
+    for element in userid_elements:
+        directory = element.attrib["directory"]
+        value = element.text
+
+        # If the directory isn't a URL, then there it is not possible to
+        # convert the value to a URL so skip this element
+        if not is_uri(directory):
+            continue
+
+        # If the value is not a URL, then convert it to a URL
+        if not is_uri(value):
+            new_value = urljoin(directory, value)
+            element.text = new_value
+
+    return eml
