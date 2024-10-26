3c2b54b5f3f40b62464307c48486483542c7cc76
Colin Smith <colin.smith@wisc.edu>
2024-10-25 17:02:03 -0700

    refactor: centralize EML and workbook loading for code clarity
    Introduce utility functions to streamline the loading of EML and
    workbook data from various input formats (e.g., file paths, DataFrames).
    This centralizes common data loading operations, improving code
    readability and maintainability.

--- tests/test_utilities.py
+++ tests/test_utilities.py
@@ -1,8 +1,10 @@
 """Test utilities module"""
 
 from lxml import etree
+import pandas as pd
 from spinneret import datasets
-from spinneret.utilities import delete_empty_tags, is_url
+from spinneret.utilities import delete_empty_tags, is_url, load_workbook, load_eml
+from spinneret.datasets import get_example_eml_dir
 
 
 def test_delete_empty_tags():
@@ -29,3 +31,30 @@ def test_is_uri():
     """Test that a string is a URL or not"""
     assert is_url("http://purl.dataone.org/odo/ECSO_00001203") is True
     assert is_url("A free text description.") is False
+
+
+def test_load_workbook():
+    """Test that a workbook is loaded as a DataFrame"""
+
+    # Input can be a file path
+    wb = load_workbook("tests/edi.3.9_annotation_workbook.tsv")
+    assert isinstance(wb, pd.core.frame.DataFrame)
+
+    # Input can be a DataFrame
+    wb = pd.read_csv("tests/edi.3.9_annotation_workbook.tsv", sep="\t")
+    wb = load_workbook(wb)
+    assert isinstance(wb, pd.core.frame.DataFrame)
+
+
+# pylint: disable=protected-access
+def test_load_eml():
+    """Test that an EML file is loaded as an ElementTree"""
+
+    # Input can be a file path
+    eml = load_eml(get_example_eml_dir() + "/" + "edi.3.9.xml")
+    assert isinstance(eml, etree._ElementTree)
+
+    # Input can be an ElementTree
+    eml = etree.parse(get_example_eml_dir() + "/" + "edi.3.9.xml")
+    eml = load_eml(eml)
+    assert isinstance(eml, etree._ElementTree)
